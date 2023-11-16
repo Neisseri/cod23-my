bceabf22ba3b3fb4b954082a5659a8787d35e372
Neisseri <neissertong@gmail.com>
2023-11-16 08:33:38 +0800

    pass compile

--- thinpad_top.srcs/sources_1/new/thinpad_top.sv
+++ thinpad_top.srcs/sources_1/new/thinpad_top.sv
@@ -80,6 +80,136 @@ module thinpad_top (
     output wire       video_de      // 行数据有效信号，用于区分消隐区
 );
 
+  /* =========== Demo code begin =========== */
+
+  // PLL 分频示例
+  logic locked, clk_10M, clk_20M;
+  pll_example clock_gen (
+      // Clock in ports
+      .clk_in1(clk_50M),  // 外部时钟输入
+      // Clock out ports
+      .clk_out1(clk_10M),  // 时钟输出 1，频率在 IP 配置界面中设置
+      .clk_out2(clk_20M),  // 时钟输出 2，频率在 IP 配置界面中设置
+      // Status and control signals
+      .reset(reset_btn),  // PLL 复位输入
+      .locked(locked)  // PLL 锁定指示输出，"1"表示时钟稳定，
+                       // 后级电路复位信号应当由它生成（见下）
+  );
+
+  logic reset_of_clk10M;
+  // 异步复位，同步释放，将 locked 信号转为后级电路的复位 reset_of_clk10M
+  always_ff @(posedge clk_10M or negedge locked) begin
+    if (~locked) reset_of_clk10M <= 1'b1;
+    else reset_of_clk10M <= 1'b0;
+  end
+
+  always_ff @(posedge clk_10M or posedge reset_of_clk10M) begin
+    if (reset_of_clk10M) begin
+      // Your Code
+    end else begin
+      // Your Code
+    end
+  end
+
+  // 不使用内存、串口时，禁用其使能信号
+  assign base_ram_ce_n = 1'b1;
+  assign base_ram_oe_n = 1'b1;
+  assign base_ram_we_n = 1'b1;
+
+  assign ext_ram_ce_n = 1'b1;
+  assign ext_ram_oe_n = 1'b1;
+  assign ext_ram_we_n = 1'b1;
+
+  assign uart_rdn = 1'b1;
+  assign uart_wrn = 1'b1;
+
+  // 数码管连接关系示意图，dpy1 同理
+  // p=dpy0[0] // ---a---
+  // c=dpy0[1] // |     |
+  // d=dpy0[2] // f     b
+  // e=dpy0[3] // |     |
+  // b=dpy0[4] // ---g---
+  // a=dpy0[5] // |     |
+  // f=dpy0[6] // e     c
+  // g=dpy0[7] // |     |
+  //           // ---d---  p
+
+  // 7 段数码管译码器演示，将 number 用 16 进制显示在数码管上面
+  logic [7:0] number;
+  SEG7_LUT segL (
+      .oSEG1(dpy0),
+      .iDIG (number[3:0])
+  );  // dpy0 是低位数码管
+  SEG7_LUT segH (
+      .oSEG1(dpy1),
+      .iDIG (number[7:4])
+  );  // dpy1 是高位数码管
+
+  logic [15:0] led_bits;
+  assign leds = led_bits;
+
+  always_ff @(posedge push_btn or posedge reset_btn) begin
+    if (reset_btn) begin  // 复位按下，设置 LED 为初始值
+      led_bits <= 16'h1;
+    end else begin  // 每次按下按钮开关，LED 循环左移
+      led_bits <= {led_bits[14:0], led_bits[15]};
+    end
+  end
+
+  // 直连串口接收发送演示，从直连串口收到的数据再发送出去
+  logic [7:0] ext_uart_rx;
+  logic [7:0] ext_uart_buffer, ext_uart_tx;
+  logic ext_uart_ready, ext_uart_clear, ext_uart_busy;
+  logic ext_uart_start, ext_uart_avai;
+
+  assign number = ext_uart_buffer;
+
+  // 接收模块，9600 无检验位
+  async_receiver #(
+      .ClkFrequency(50000000),
+      .Baud(9600)
+  ) ext_uart_r (
+      .clk           (clk_50M),         // 外部时钟信号
+      .RxD           (rxd),             // 外部串行信号输入
+      .RxD_data_ready(ext_uart_ready),  // 数据接收到标志
+      .RxD_clear     (ext_uart_clear),  // 清除接收标志
+      .RxD_data      (ext_uart_rx)      // 接收到的一字节数据
+  );
+
+  assign ext_uart_clear = ext_uart_ready; // 收到数据的同时，清除标志，因为数据已取到 ext_uart_buffer 中
+  always_ff @(posedge clk_50M) begin  // 接收到缓冲区 ext_uart_buffer
+    if (ext_uart_ready) begin
+      ext_uart_buffer <= ext_uart_rx;
+      ext_uart_avai   <= 1;
+    end else if (!ext_uart_busy && ext_uart_avai) begin
+      ext_uart_avai <= 0;
+    end
+  end
+  always_ff @(posedge clk_50M) begin  // 将缓冲区 ext_uart_buffer 发送出去
+    if (!ext_uart_busy && ext_uart_avai) begin
+      ext_uart_tx <= ext_uart_buffer;
+      ext_uart_start <= 1;
+    end else begin
+      ext_uart_start <= 0;
+    end
+  end
+
+  // 图像输出演示，分辨率 800x600@75Hz，像素时钟为 50MHz
+  logic [11:0] hdata;
+  assign video_red   = hdata < 266 ? 3'b111 : 0;  // 红色竖条
+  assign video_green = hdata < 532 && hdata >= 266 ? 3'b111 : 0;  // 绿色竖条
+  assign video_blue  = hdata >= 532 ? 2'b11 : 0;  // 蓝色竖条
+  assign video_clk   = clk_50M;
+  vga #(12, 800, 856, 976, 1040, 600, 637, 643, 666, 1, 1) vga800x600at75 (
+      .clk        (clk_50M),
+      .hdata      (hdata),        // 横坐标
+      .vdata      (),             // 纵坐标
+      .hsync      (video_hsync),
+      .vsync      (video_vsync),
+      .data_enable(video_de)
+  );
+  /* =========== Demo code end =========== */
+
   logic sys_clk;
   logic sys_rst;
 
@@ -150,14 +280,17 @@ module thinpad_top (
   // EXE stage
   reg [31:0] rf_writeback_reg;
 
+  //  load: https://blog.csdn.net/sucaiwa/article/details/129328907
+  // store: https://blog.csdn.net/sucaiwa/article/details/129328939
+
   //       [14:12] [6:0]
-  //  LUI:         0110111
+  //  LUI:         0110111 √
 
   //  BEQ:     000 1100011
 
-  //   LB:     000 0000011
+  //   LB:     000 0000011 √
 
-  //   SB:     000 0100011
+  //   SB:     000 0100011 √
   //   SW:     010 0100011 √
 
   // ADDI:     000 0010011 √
@@ -183,15 +316,24 @@ module thinpad_top (
       STATE_ID: begin
         if (inst_reg[6:0] == 7'b0010011) begin // ADDI & ANDI
           rf_raddr_a_o = inst_reg[19:15]; // rs1
-          imm = inst_reg[31:20];
+          imm = $signed(inst_reg[31:20]);
         end else if (inst_reg[6:0] == 7'b0110011) begin // ADD
           rf_raddr_a_o = inst_reg[19:15]; // rs1
           rf_raddr_b_o = inst_reg[24:20]; // rs2
-        end else if (inst_reg[6:0] == 7'b0100011 && inst_reg[14:12] == 3'b010) begin // SW
+        end else if (inst_reg[6:0] == 7'b0100011) begin // SW & SB
           rf_raddr_a_o = inst_reg[19:15]; // rs1
           rf_raddr_b_o = inst_reg[24:20]; // rs2
-          imm = inst_reg[11:7] + (inst_reg[31:25] << 5);
+          imm = $signed(inst_reg[11:7] + (inst_reg[31:25] << 5));
           //             [4:0]   [11:5]
+        end else if (inst_reg[6:0] == 7'b0000011) begin // LB
+          rf_raddr_a_o = inst_reg[19:15]; // rs1
+          imm = $signed(inst_reg[31:20]);
+        end else if (inst_reg[6:0] == 7'b0110111) begin // LUI
+          imm = {inst_reg[31:12], 12'h000};
+        end else if (inst_reg[6:0] == 7'b1100011) begin // BEQ
+          rf_raddr_a_o = inst_reg[19:15]; // rs1
+          rf_raddr_b_o = inst_reg[24:20]; // rs2
+          imm = $signed({inst_reg[31], inst_reg[7], inst_reg[30:25], inst_reg[11:8], 1'b0});
         end
       end
 
@@ -202,19 +344,36 @@ module thinpad_top (
           alu_op_o = 4'b0001; // ALU_ADD
         end else if (inst_reg[6:0] == 7'b0010011 && inst_reg[14:12] == 3'b111) begin // ANDI
           alu_op_o = 4'b0011; // ALU_AND
-        end else if (inst_reg[6:0] == 7'b0100011 && inst_reg[14:12] == 3'b010) begin // SW
+        end else if (inst_reg[6:0] == 7'b0100011 || inst_reg[6:0] == 7'b0000011) begin // SW & SB & LB
           alu_op_o = 4'b0001; // ALU_ADD
         end
       end
 
       STATE_MEM: begin
         if (inst_reg[6:0] == 7'b0100011 && inst_reg[14:12] == 3'b010) begin // SW
-          top_adr_o = alu_result_i; // [rs1] + imm
+          top_adr_o = 32'h8000_0000 + alu_result_i * 4; // BaseRAM address: [rs1] + imm
+          // note that in RAM, an address represents 32 bits
           top_dat_o = rf_rdata_b_i; // rs2
+          top_sel_o = 4'b1111;
+
           top_cyc_o = 1'b1;
           top_stb_o = 1'b1;
           top_we_o = 1'b1; // write
-          top_sel_o = 4'b1111;
+        end else if (inst_reg[6:0] == 7'b0100011 && inst_reg[14:12] == 3'b000) begin // SB
+          top_adr_o = 32'h8000_0000 + alu_result_i * 4; // [rs1] + imm
+          top_dat_o = rf_rdata_b_i[7:0]; // rs2
+          top_sel_o = 4'b0001;
+
+          top_cyc_o = 1'b1;
+          top_stb_o = 1'b1;
+          top_we_o = 1'b1; // write
+        end else if (inst_reg[6:0] == 7'b0000011) begin // LB
+          top_adr_o = 32'h8000_0000 + alu_result_i * 4; // [rs1] + imm
+          top_sel_o = 4'b0001;
+          
+          top_cyc_o = 1'b1;
+          top_stb_o = 1'b1;
+          top_we_o = 1'b0; // read
         end
       end
 
@@ -222,6 +381,14 @@ module thinpad_top (
         if (inst_reg[6:0] == 7'b0010011 || inst_reg[6:0] == 7'b0110011) begin // ADDI & ANDI & ADD
           rf_we_o = 1'b1;
           rf_wdata_o = rf_writeback_reg;
+          rf_waddr_o = inst_reg[11:7]; // rd
+        end else if (inst_reg[6:0] == 7'b0000011) begin // LB
+          rf_we_o = 1'b1;
+          rf_wdata_o = rf_writeback_reg;
+          rf_waddr_o = inst_reg[11:7]; // rd
+        end else if (inst_reg[6:0] == 7'b0110111) begin // LUI
+          rf_we_o = 1'b1;
+          rf_wdata_o = imm;
           rf_waddr_o = inst_reg[11:7]; // rd
         end
       end
@@ -235,11 +402,12 @@ module thinpad_top (
     case (state)
 
       STATE_IF: begin
-        inst_reg <= top_dat_i;
-        top_cyc_o <= 1'b0;
-        top_stb_o <= 1'b0;
-        pc_now_reg <= pc_reg; // pc_now_reg: this instr, pc_reg: next instr
-        if (wb_ack_i) begin // wishbone ack: PC + 4 get
+        if (top_ack_i == 1'b1) begin // reading inst finished
+          inst_reg <= top_dat_i;
+          // top_cyc_o <= 1'b0;
+          // top_stb_o <= 1'b0;
+
+          pc_now_reg <= pc_reg; // pc_now_reg: this instr, pc_reg: next instr
           pc_reg <= alu_result_i; // hold `addr` when wishbone request
           state <= STATE_ID;
         end
@@ -248,28 +416,50 @@ module thinpad_top (
       STATE_ID: begin
         if (inst_reg[6:0] == 7'b0010011) begin // ADDI $ ANDI
           operand1_reg <= rf_rdata_a_i; // rs1
-          operand2_reg <= imm; // rd
+          operand2_reg <= imm; // imm
+          state <= STATE_EXE;
         end else if (inst_reg[6:0] == 7'b0110011) begin // ADD
           operand1_reg <= rf_rdata_a_i; // rs1
           operand2_reg <= rf_rdata_b_i; // rs2
-        end else if (inst_reg[6:0] == 7'b0100011 && inst_reg[14:12] == 3'b010) begin // SW
+          state <= STATE_EXE;
+        end else if (inst_reg[6:0] == 7'b0100011) begin // SW & SB
           operand1_reg <= rf_rdata_a_i; // rs1
-          operand2_reg <= $signed(imm); // signed extension
+          operand2_reg <= imm; // signed extension
+          state <= STATE_EXE;
+        end else if (inst_reg[6:0] == 7'b0000011) begin // LB
+          operand1_reg <= rf_rdata_a_i; // rs1
+          operand2_reg <= imm;
+          state <= STATE_EXE;
+        end else if (inst_reg[6:0] == 7'b0110111) begin // LUI
+          state <= STATE_WB;
+        end else if (inst_reg[6:0] == 7'b1100011) begin // BEQ
+          if (rf_rdata_a_i == rf_rdata_b_i) begin
+            pc_reg <= pc_now_reg + imm;
+          end
+          state <= STATE_IF;
         end
-        state <= STATE_EXE;
       end
 
       STATE_EXE: begin
         if (inst_reg[6:0] == 7'b0010011 || inst_reg[6:0] == 7'b0110011) begin // ADDI & ANDI & ADD
           rf_writeback_reg <= alu_result_i;
           state <= STATE_WB;
-        end else if (inst_reg[6:0] == 7'b0100011 && inst_reg[14:12] == 3'b010) begin // SW
+        end else if (inst_reg[6:0] == 7'b0100011 || inst_reg[6:0] == 7'b0000011) begin // SW & SB & LB
           state <= STATE_MEM;
         end
       end
 
       STATE_MEM: begin
-        state <= STATE_IF;
+        if (top_ack_i == 1'b1) begin // write or read is finished
+          if (inst_reg[6:0] == 7'b0100011) begin // SW & SB
+            // top_cyc_o <= 1'b0;
+            // top_stb_o <= 1'b0;
+            state <= STATE_IF;
+          end else if (inst_reg[6:0] == 7'b0000011) begin // LB
+            rf_writeback_reg <= $signed(top_dat_i[7:0]);
+            state <= STATE_WB;
+          end
+        end
       end
 
       STATE_WB: begin
